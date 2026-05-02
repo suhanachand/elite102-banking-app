e18833102fabc6a6f4eb2967a4f6129be5724436
Suhana Chand <suhanachand03@gmail.com>
2026-05-01 19:29:50 -0700

    Add deposit, withdraw, check balance, and list accounts

--- main.py
+++ main.py
@@ -16,5 +16,19 @@ while True:
         deposit = float(input("Initial deposit: "))
         banking.create_account(name, deposit)
 
+    elif choice == "2":
+        acc_id = int(input("Account ID: "))
+        amount = float(input("Amount: "))
+        banking.deposit(acc_id, amount)
+
+    elif choice == "3":
+        acc_id = int(input("Account ID: "))
+        amount = float(input("Amount: "))
+        banking.withdraw(acc_id, amount)
+
+    elif choice == "4":
+        acc_id = int(input("Account ID: "))
+        banking.check_balance(acc_id)
+
     elif choice == "5":
-        break
+        banking.list_accounts()
