ca8c1ccf81e91f529a1065b167ca9d91962b0f84
Suhana Chand <suhanachand03@gmail.com>
2026-05-01 19:26:26 -0700

    Add create_account function to banking.py

--- banking.py
+++ banking.py
@@ -1,1 +1,16 @@
 
+from database import connect
+
+def create_account(name, deposit):
+    conn = connect()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "INSERT INTO accounts (name, balance) VALUES (?, ?)",
+        (name, deposit)
+    )
+
+    conn.commit()
+    conn.close()
+
+    print("Account created!")
