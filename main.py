3ce8c27f0be1d4c11e17b60211cb107165dc6397
Suhana Chand <suhanachand03@gmail.com>
2026-05-01 21:38:50 -0700

    Refactor banking system with screen clearing and transfer

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 from database import create_tables
 import banking
+import os
 
 create_tables()
-print("Loading Banking System...")
-print("=== Welcome to Banking System ===")
+
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
 
 while True:
-    print("\n1. Create Account")
+    clear_screen()
+
+    print("BANKING SYSTEM")
+
+    # ALWAYS SHOW TABLE FIRST
+    banking.show_accounts_table()
+
+    print("\nmenu")
+    print("1. Create Account")
     print("2. Deposit")
     print("3. Withdraw")
     print("4. Check Balance")
-    print("5. List Accounts")
+    print("5. Transfer Money")
     print("6. Exit")
 
-    choice = input("Enter choice: ")
+    choice = input("\nEnter choice: ")
 
     if choice == "1":
         name = input("Name: ")
@@ -33,13 +43,19 @@ while True:
     elif choice == "4":
         acc_id = int(input("Account ID: "))
         banking.check_balance(acc_id)
+        input("press enter to continue")
 
     elif choice == "5":
-        banking.list_accounts()
+        from_id = int(input("From Account ID: "))
+        to_id = int(input("To Account ID: "))
+        amount = float(input("Amount: "))
+        banking.transfer(from_id, to_id, amount)
 
     elif choice == "6":
         print("Goodbye!")
         break
 
     else:
-        print("Invalid choice. Try again.")
+        print("Invalid choice")
+
+    input("\npress enter to continue...")
